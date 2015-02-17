5355342f03642f2c6986a20accd053dac7078a69
dawkinrm <dawkinrm@dukes.jmu.edu>
2015-02-17 16:17:18 -0500

    Lab 3, Part 1, Step 6, 7, & 8. Further functionality added

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ from kivy.app import App
 from kivy.core.audio import SoundLoader
 
 class HelloWorldApp (App):
+	stopped = True
 	def play_music(self):
-		self.sound = SoundLoader.load('Jesse_Cook-Dance_of_Spring.ogg')
-		self.sound.play()
+		if HelloWorldApp.stopped is True:
+			self.sound = SoundLoader.load('Jesse_Cook-Dance_of_Spring.ogg')
+			self.sound.play()
+			HelloWorldApp.stopped = False
+	
+	def stop_music(self):
+		self.sound.stop()
+		HelloWorldApp.stopped = True
+	
+	def volume_change(self, volume):
+		if self.sound.state is 'play' or self.sound.state is 'stop':
+			self.sound.volume = volume
 
 if __name__ == "__main__":
 	HelloWorldApp().run()
