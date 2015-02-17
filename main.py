bdd9b6f534354f4b2e39141942f92b4b0b2cb86f
dawkinrm <dawkinrm@dukes.jmu.edu>
2015-02-17 14:25:09 -0500

    Lab 3, Part 1, Step 2. Addition of music playing function on button click

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from kivy.app import App
+from kivy.core.audio import SoundLoader
 
 class HelloWorldApp (App):
-	pass
+	def play_music(self):
+		self.sound = SoundLoader.load('Jesse_Cook-Dance_of_Spring.ogg')
+		self.sound.play()
 
 if __name__ == "__main__":
 	HelloWorldApp().run()
